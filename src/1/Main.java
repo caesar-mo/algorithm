8c71e3fb4779a2b2765b3a943c5f3aae59520401
caesar-mo <59224354+caesar-mo@users.noreply.github.com>
2020-03-06 17:57:15 +0900

    1번 문제 손익분기점

---
+++ src/1/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/*
+ * 문제 출처 : https://www.acmicpc.net/problem/1712
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int st = sc.nextInt();
+		int dy = sc.nextInt();
+		int sr = sc.nextInt();
+		int tp = -1;
+		
+		if(dy<sr)
+			tp = st/(sr-dy) + 1;
+		
+		System.out.println(tp);
+	}
+
+}
